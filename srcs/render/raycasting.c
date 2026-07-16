4df98477f5880df316c835e9008e6340b5cbc9d0
lilypad <ilyyapinglang1024@outlook.com>
2026-07-16 18:06:03 +0200

    split raycasting into clear functions

--- srcs/render/raycasting.c
+++ srcs/render/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: ylang <ylang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/15 17:15:32 by ylang             #+#    #+#             */
-/*   Updated: 2026/07/15 18:55:17 by ylang            ###   ########.fr       */
+/*   Updated: 2026/07/16 18:04:45 by ylang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,160 +18,186 @@
  raycast every column
 		↓
 	draw wall slice
-
 */
+void	init_ray_params(int x, t_game *game)
+{
+	double	cameraX;
+
+	// calculate ray position and direction
+	cameraX = 2 * x / (double)WIN_W - 1;
+	// x-coordinate in camera space
+	game->ray.rayDirX = game->player.dir_x + game->player.plane_x * cameraX;
+	game->ray.rayDirY = game->player.dir_y + game->player.plane_y * cameraX;
+	// which box of the map we're in
+	game->ray.mapX = (int)(game->player.pos_x);
+	game->ray.mapY = (int)(game->player.pos_y);
+	game->ray.deltaDistX = (game->ray.rayDirX == 0) ? 1e30 : fabs(1
+			/ game->ray.rayDirX);
+	game->ray.deltaDistY = (game->ray.rayDirY == 0) ? 1e30 : fabs(1
+			/ game->ray.rayDirY);
+	// what direction to step in x or y-direction (either +1 or -1)
+	game->ray.hit = 0;  // was there a wall hit?
+	game->ray.side = 0; // was a NS or a EW wall hit?
+						// calculate step and initial sideDist
+}
+
+// set ray related params like stepx, stepy, sideDistX, deltaDistX, etc.
+void	cal_step_n_sideDist(t_game *game)
+{
+	if (game->ray.rayDirX < 0)
+	{
+		game->ray.stepX = -1;
+		game->ray.sideDistX = (game->player.pos_x - game->ray.mapX)
+			* game->ray.deltaDistX;
+	}
+	else
+	{
+		game->ray.stepX = 1;
+		game->ray.sideDistX = (game->ray.mapX + 1.0 - game->player.pos_x)
+			* game->ray.deltaDistX;
+	}
+	if (game->ray.rayDirY < 0)
+	{
+		game->ray.stepY = -1;
+		game->ray.sideDistY = (game->player.pos_y - game->ray.mapY)
+			* game->ray.deltaDistY;
+	}
+	else
+	{
+		game->ray.stepY = 1;
+		game->ray.sideDistY = (game->ray.mapY + 1.0 - game->player.pos_y)
+			* game->ray.deltaDistY;
+	}
+}
+
+void	perform_dda(t_game *game)
+{
+	while (game->ray.hit == 0)
+	{
+		// jump to next map square, either in x-direction,
+		// or in y - direction
+		if (game->ray.sideDistX < game->ray.sideDistY)
+		{
+			game->ray.sideDistX += game->ray.deltaDistX;
+			game->ray.mapX += game->ray.stepX;
+			game->ray.side = 0;
+		}
+		else
+		{
+			game->ray.sideDistY += game->ray.deltaDistY;
+			game->ray.mapY += game->ray.stepY;
+			game->ray.side = 1;
+		}
+		// Check if ray has hit a wall
+		if (game->map.grid[game->ray.mapY][game->ray.mapX] == '1')
+			game->ray.hit = 1;
+	}
+}
+
+// Calculate height of line to draw on screen
+// calculate lowest and highest pixel to fill in current stripe
+void	calculate_strip(t_game *game, int pitch)
+{
+	if (game->ray.side == 0)
+		game->ray.perpWallDist = (game->ray.sideDistX - game->ray.deltaDistX);
+	else
+		game->ray.perpWallDist = (game->ray.sideDistY - game->ray.deltaDistY);
+	// Calculate height of line to draw on screen
+	game->ray.lineHeight = (int)(WIN_H / game->ray.perpWallDist);
+	// calculate lowest and highest pixel to fill in current stripe
+	game->ray.drawStart = -game->ray.lineHeight / 2 + WIN_H / 2 + pitch;
+	if (game->ray.drawStart < 0)
+		game->ray.drawStart = 0;
+	game->ray.drawEnd = game->ray.lineHeight / 2 + WIN_H / 2 + pitch;
+	if (game->ray.drawEnd >= WIN_H)
+		game->ray.drawEnd = WIN_H - 1;
+}
+
+double	get_wall_hit_pos(t_game *game)
+{
+	double wallX; // where exactly the wall was hit
+	if (game->ray.side == 0)
+		wallX = game->player.pos_y + game->ray.perpWallDist * game->ray.rayDirY;
+	else
+		wallX = game->player.pos_x + game->ray.perpWallDist * game->ray.rayDirX;
+	wallX -= floor((wallX));
+	return (wallX);
+}
+void	draw_texture_by_pixel(t_game *game, int pitch, int texNum, int x)
+{
+	int				texX;
+	double			step;
+	double			texPos;
+	int				texY;
+	unsigned int	color;
+
+	double wallX; // where exactly the wall was hit
+	if (game->ray.side == 0)
+		wallX = game->player.pos_y + game->ray.perpWallDist * game->ray.rayDirY;
+	else
+		wallX = game->player.pos_x + game->ray.perpWallDist * game->ray.rayDirX;
+	wallX -= floor((wallX));
+	texX = (int)(wallX * (double)game->tex->width);
+	if (game->ray.side == 0 && game->ray.rayDirX > 0)
+		texX = game->tex->width - texX - 1;
+	if (game->ray.side == 1 && game->ray.rayDirY < 0)
+		texX = game->tex->width - texX - 1;
+	step = 1.0 * game->tex->height / game->ray.lineHeight;
+	texPos = (game->ray.drawStart - pitch - WIN_H / 2 + game->ray.lineHeight
+			/ 2) * step;
+	for (int y = game->ray.drawStart; y < game->ray.drawEnd; y++)
+	{
+		texY = (int)texPos & (game->tex->height - 1);
+		texPos += step;
+		color = *(int *)(game->tex[texNum].data + texY
+				* game->tex[texNum].line_len + texX * (game->tex[texNum].bpp
+					/ 8));
+		if (game->ray.side == 1)
+			color = (color >> 1) & 8355711;
+		put_pixel(&game->img, x, y, color);
+	}
+}
+
+int	texture_calculations(t_game *game)
+{
+	int	texNum;
+
+	if (game->ray.side == 0)
+	{
+		if (game->ray.rayDirX > 0)
+			texNum = WEST_TEX;
+		else
+			texNum = EAST_TEX;
+	}
+	else
+	{
+		if (game->ray.rayDirY > 0)
+			texNum = NORTH_TEX;
+		else
+			texNum = SOUTH_TEX;
+	}
+	return (texNum);
+}
 
 void	raycasting(t_game *game)
 {
-	double rayDirX;
-	double rayDirY;
-	int mapX;
-	int mapY;
-	double sideDistX;
-	double sideDistY;
-	double deltaDistX;
-	double deltaDistY;
-	double perpWallDist;
-	int stepX;
-	int stepY;
-	int lineHeight;
-	int drawStart;
-	int drawEnd;
-	double cameraX;
-	int pitch;
+	int	pitch;
+	int	texNum;
+	int	x;
 
-	int texX;
-	double step;
-	double texPos;
-	int texY;
-	unsigned int color;
-	int texNum;
-
-	int x = 0;
+	// int	wallX;
+	x = 0;
 	while (x < WIN_W)
 	{
-		// calculate ray position and direction
-		cameraX = 2 * x / (double)WIN_W - 1;
-		// x-coordinate in camera space
-		rayDirX = game->player.dir_x + game->player.plane_x * cameraX;
-		rayDirY = game->player.dir_y + game->player.plane_y * cameraX;
-		// which box of the map we're in
-		mapX = (int)(game->player.pos_x);
-		mapY = (int)(game->player.pos_y);
-
-		deltaDistX = (rayDirX == 0) ? 1e30 : fabs(1 / rayDirX);
-		deltaDistY = (rayDirY == 0) ? 1e30 : fabs(1 / rayDirY);
-		// what direction to step in x or y-direction (either +1 or -1)
-		int hit = 0; // was there a wall hit?
-		int side;    // was a NS or a EW wall hit?
-		// calculate step and initial sideDist
-		if (rayDirX < 0)
-		{
-			stepX = -1;
-			sideDistX = (game->player.pos_x - mapX) * deltaDistX;
-		}
-		else
-		{
-			stepX = 1;
-			sideDistX = (mapX + 1.0 - game->player.pos_x) * deltaDistX;
-		}
-		if (rayDirY < 0)
-		{
-			stepY = -1;
-			sideDistY = (game->player.pos_y - mapY) * deltaDistY;
-		}
-		else
-		{
-			stepY = 1;
-			sideDistY = (mapY + 1.0 - game->player.pos_y) * deltaDistY;
-		}
-		// perform DDA
-		while (hit == 0)
-		{
-			// jump to next map square, either in x-direction,
-			// or in y - direction
-			if (sideDistX < sideDistY)
-			{
-				sideDistX += deltaDistX;
-				mapX += stepX;
-				side = 0;
-			}
-			else
-			{
-				sideDistY += deltaDistY;
-				mapY += stepY;
-				side = 1;
-			}
-			// Check if ray has hit a wall
-			if (game->map.grid[mapY][mapX] == '1')
-				hit = 1;
-		}
-
-		if (side == 0)
-			perpWallDist = (sideDistX - deltaDistX);
-		else
-			perpWallDist = (sideDistY - deltaDistY);
-		// Calculate height of line to draw on screen
-		lineHeight = (int)(WIN_H / perpWallDist);
-
+		init_ray_params(x, game);
+		cal_step_n_sideDist(game);
+		perform_dda(game);
 		pitch = 100;
-		// calculate lowest and highest pixel to fill in current stripe
-		drawStart = -lineHeight / 2 + WIN_H / 2 + pitch;
-		if (drawStart < 0)
-			drawStart = 0;
-		drawEnd = lineHeight / 2 + WIN_H / 2 + pitch;
-		if (drawEnd >= WIN_H)
-			drawEnd = WIN_H - 1;
+		calculate_strip(game, pitch);
 		// texturing calculations
-		if (side == 0)
-		{
-			if (rayDirX > 0)
-				texNum = WEST_TEX; // 3west
-			else
-				texNum = EAST_TEX; // 2esst
-		}
-		else
-		{
-			if (rayDirY > 0)
-				texNum = NORTH_TEX; // 0south
-			else
-				texNum = SOUTH_TEX; // 1north
-		}
-		// 1 subtracted from it so that texture 0 can be used!
-		// calculate value of wallX
-		double wallX; // where exactly the wall was hit
-		if (side == 0)
-			wallX = game->player.pos_y + perpWallDist * rayDirY;
-		else
-			wallX = game->player.pos_x + perpWallDist * rayDirX;
-		wallX -= floor((wallX));
-		// x coordinate on the texture
-		texX = (int)(wallX * (double)game->tex->width);
-		if (side == 0 && rayDirX > 0)
-			texX = game->tex->width - texX - 1;
-		if (side == 1 && rayDirY < 0)
-			texX = game->tex->width - texX - 1;
-		// TODO: an integer-only bresenham or DDA like algorithm could make the texture coordinate stepping faster
-		// How much to increase the texture coordinate per screen pixel
-		step = 1.0 * game->tex->height / lineHeight;
-		// Starting texture coordinate
-		texPos = (drawStart - pitch - WIN_H / 2 + lineHeight / 2) * step;
-		for (int y = drawStart; y < drawEnd; y++)
-		{
-			// Cast the texture coordinate to integer, and mask with (texHeight
-			//	- 1) in case of overflow
-			texY = (int)texPos & (game->tex->height - 1);
-			texPos += step;
-			// color = game->tex[texNum].pixels[texY * game->tex[texNum].width
-			// 	+ texX];
-			int offset = texY * game->tex[texNum].line_len + texX
-				* (game->tex[texNum].bpp / 8);
-			color = *(int *)(game->tex[texNum].data + offset);
-
-			// make color darker for y-sides: R,
-			// G and B byte each divided through two with a "shift" and an "and" if (side == 1) color = (color >> 1) & 8355711;
-			put_pixel(&game->img, x, y, color);
-		}
+		texNum = texture_calculations(game);
+		// wallX = get_wall_hit_pos(game);
+		draw_texture_by_pixel(game, pitch, texNum, x);
 		x++;
 	}
 }
