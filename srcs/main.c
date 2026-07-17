afb0960576888508007a7675f411b5007a84b51b
lilypad <ilyyapinglang1024@outlook.com>
2026-07-17 16:07:48 +0200

    integrate the code, test with real cub file

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ylang <ylang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/15 17:17:50 by ylang             #+#    #+#             */
-/*   Updated: 2026/07/16 19:27:01 by ylang            ###   ########.fr       */
+/*   Updated: 2026/07/17 15:39:52 by ylang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,16 @@ void	init_fake_texture_from_file(t_game *game)
 {
 	game->tex[NORTH_TEX].height = 64;
 	game->tex[NORTH_TEX].width = 64;
-	game->tex[NORTH_TEX].path = "./texture/north64.xpm";
+	// game->tex[NORTH_TEX].path = "./texture/north64.xpm";
 	game->tex[SOUTH_TEX].height = 64;
 	game->tex[SOUTH_TEX].width = 64;
-	game->tex[SOUTH_TEX].path = "./texture/south64.xpm";
+	// game->tex[SOUTH_TEX].path = "./texture/south64.xpm";
 	game->tex[WEST_TEX].height = 64;
 	game->tex[WEST_TEX].width = 64;
-	game->tex[WEST_TEX].path = "./texture/west64.xpm";
+	// game->tex[WEST_TEX].path = "./texture/west64.xpm";
 	game->tex[EAST_TEX].height = 64;
 	game->tex[EAST_TEX].width = 64;
-	game->tex[EAST_TEX].path = "./texture/east64.xpm";
+	// game->tex[EAST_TEX].path = "./texture/east64.xpm";
 	load_all_textures(game);
 }
 
@@ -98,16 +98,15 @@ void	load_player(t_game *game)
 
 int	fake_data(t_game *game)
 {
-	static char	*map[] = {"111111111111", "100N01000001", "111010111101",
-			"100010000101", "101111010101", "100000010001", "101111111101",
-			"100000000001", "111111111111", NULL};
-
-	game->map.grid = map;
-	game->map.height = 9;
-	game->map.width = 12;
+	// static char	*map[] = {"111111111111", "100N01000001", "111010111101",
+	// 		"100010000101", "101111010101", "100000010001", "101111111101",
+	// 		"100000000001", "111111111111", NULL};
+	// game->map.grid = map;
+	// game->map.height = 9;
+	// game->map.width = 12;
 	load_player(game);
-	game->floor = 0xFFC5D3;   // LIGHT PINK
-	game->ceiling = 0x90D5FF; // LIGHT BLUE
+	// game->floor = 0xFFC5D3;   // LIGHT PINK
+	// game->ceiling = 0x90D5FF; // LIGHT BLUE
 	game->keys.key_a = false;
 	game->keys.key_d = false;
 	game->keys.key_s = false;
@@ -122,14 +121,18 @@ int	main(int argc, char **argv)
 {
 	t_game	game;
 
-	// if (argc != 2)
-	// 	return (err_msg("", ERR_USAGE, 1));
+	if (argc != 2)
+		return (err_msg("", ERR_USAGE, 1));
 	(void)argc;
 	(void)argv;
 	init_all(&game);
 	init_mlx(&game);
-	// if (parse_file(&game, argv))
-	// 	return (1);
+	if (parse_file(&game, argv))
+		return (1);
+	printf("map width is : %d, map height is :%d \n", game.map.width,
+		game.map.height);
+	printf("tex 0.1.2.3 path %s, %s, %s, %s\n", game.tex[0].path,
+		game.tex[1].path, game.tex[2].path, game.tex[2].path);
 	fake_data(&game);
 	// 1. keyboard events 2. mouse events 3. a part of window needs to be redrawn (expose)
 	set_hooks(&game);
